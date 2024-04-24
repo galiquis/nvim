399362976bd97ca98eb326640f905bd83e6ebe8e
galiquis <galiquis@gmail.com>
2024-04-25 00:01:54 +0100

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   lazy-lock.json 	new file:   lua/plugins/lspconfig.lua 	renamed:    lua/plugins/lsp-config.lua -> old/lsp-config.lua Swapped lsp-config for the file used in Kickstarter as not all lsp's were attaching.

--- old/lsp-config.lua
+++ old/lsp-config.lua
@@ -16,6 +16,18 @@ return {
     {
         "neovim/nvim-lspconfig",
         lazy = false,
+        dependencies = {
+            'WhoIsSethDaniel/mason-tool-installer.nvim',
+
+            -- Useful status updates for LSP.
+            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+            { 'j-hui/fidget.nvim', opts = {} },
+
+            -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
+            -- used for completion, annotations and signatures of Neovim apis
+            { 'folke/neodev.nvim', opts = {} },
+
+        },
         config = function()
             local lspconfig = require("lspconfig")
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
