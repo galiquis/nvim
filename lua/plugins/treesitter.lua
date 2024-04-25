0cc7afeccc96007ed0d97be54d97963d1f47b931
galiquis <galiquis@gmail.com>
2024-04-25 16:01:43 +0100

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   lua/plugins/treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,13 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function() 
+    config = function()
         -- Treesitter config
         local config = require("nvim-treesitter.configs")
         config.setup({
             ensure_installed = {"lua", "python", "javascript"},
             sync_install = false,
-        --    highlight = { enable = true },
-        --    indent = { enable = true },
-})
+            auto_install = true,
+        })
     end
 }
